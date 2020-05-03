8fb37947fcf719d1782ab595f4771d6934618dd7
David <david_borelli@live.com>
2020-05-03 17:04:30 -0300

    Add functional method

--- picme/src/services/api.js
+++ picme/src/services/api.js
@@ -1,24 +1,23 @@
 import axios from 'axios';
 import { envVariables } from '~/env';
-import  history from './history';
-
+import history from './history';
 
 const api = axios.create({
   baseURL: envVariables.API_ADDRESS,
 });
 
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  }, 
-  (error) => {
-    if (error.response.status === 401) {
-      // TODO enviar para a tela de login
-      // TODO remover o estado do usuario na aplicação
-    }
-    
-    return Promise.reject(error);
-  }
-);
+// api.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     if (error.response.status === 401) {
+//       // TODO enviar para a tela de login
+//       // TODO remover o estado do usuario na aplicação
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
