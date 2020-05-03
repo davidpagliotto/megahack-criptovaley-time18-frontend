b08bdc3a1a51bc1c5ae305865628b1e738d6a5b6
rcbull <rcbull@gmail.com>
2020-05-03 09:05:57 -0300

    fixed transaction with smart contract

--- picme/src/services/blockChain.js
+++ picme/src/services/blockChain.js
@@ -5,18 +5,18 @@ const CryptoJS = require('crypto-js');
 
 export const getCurrentProvider = () => {
   const provider = window.web3.currentProvider;
-  console.log(provider);
+  console.log('getCurrentProvider', provider);
   return provider;
 };
 
 export const getContract = () => {
-  const web3 = new Web3();
+  const web3 = new Web3(getCurrentProvider());
   const contract = new web3.eth.Contract(CONTRACT_ABI.abi, CONTRACT_ADDRESS);
   return contract;
 };
 
 export const getAccount = async () => {
-  const web3 = new Web3(window.web3.currentProvider);
+  const web3 = new Web3(getCurrentProvider());
   const account = await web3.eth.getAccounts();
   console.log('getAccount', account[0]);
   return account[0];
@@ -24,10 +24,9 @@ export const getAccount = async () => {
 
 export const getBatchCount = async () => {
   const batchCount = await getContract().methods.getBatchCount().call();
+  console.log("batchCount = ", batchCount)
   return batchCount;
 };
-
-// 0x"+CryptoJS.lib.WordArray.random(20)
 
 export const insertBatch = async (
   supplier,
@@ -36,26 +35,28 @@ export const insertBatch = async (
   document_number,
   document
 ) => {
-  // const batchParam = [
-  //   `0x${CryptoJS.lib.WordArray.random(20)}`,
-  //   `0x${CryptoJS.lib.WordArray.random(20)}`, // TODO: Depois deve ser trocado pelo do parametro
-  //   batchOrigin,
-  //   geolocation, // TODO: Depois deve ser trocado pelo do parametro
-  //   document_number,
-  //   document,
-  // ];
+  const batchParam = [
+    `0x${CryptoJS.lib.WordArray.random(20)}`,
+    `0x${CryptoJS.lib.WordArray.random(20)}`, // TODO: Depois deve ser trocado pelo do parametro
+    batchOrigin,
+    geolocation, // TODO: Depois deve ser trocado pelo do parametro
+    document_number,
+    document,
+  ];
+
+  // verifica o número de lotes existentes
+  getBatchCount()
+
+  const account = await getAccount()
+
+  console.log(account)
 
   getContract()
     .methods.insertBatch(
-      `0xfC01e72e7089E5Ac5473B9Dc8d775570067588a3`,
-      `0xfC01e72e7089E5Ac5473B9Dc8d775570067588a3`, // TODO: Depois deve ser trocado pelo do parametro
-      '0x1772976766B5C5C01EbfcACBD3C7157DDd9DCf95',
-      "{'lat':-27.5,'long':-47.5}", // geolocation
-      '001', // document_number
-      'NF' // document
+      ...batchParam
     )
     .send({
-      from: '0xA7a2fAd21628aaa224Db4c82F6062B771efb1Dfe',
+      from: account,
     })
     .on('transactionHash', (hash) => {
       // TODO: Enviar essa hash para API
