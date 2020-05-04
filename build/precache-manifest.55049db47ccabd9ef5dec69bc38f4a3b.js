2b8d5fdd6ac1f8a5c70992fa3880687c027394b2
David <david_borelli@live.com>
2020-05-04 10:04:52 -0300

    Setup build

---
+++ build/precache-manifest.55049db47ccabd9ef5dec69bc38f4a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732629586bcbd363d97ac3f228f77915",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bca7e3c1c457e1941ae6",
+    "url": "/static/css/2.cadd3b9a.chunk.css"
+  },
+  {
+    "revision": "bca7e3c1c457e1941ae6",
+    "url": "/static/js/2.901c1cd9.chunk.js"
+  },
+  {
+    "revision": "bb4dfe7f56eb12bcdb6b6307771969ff",
+    "url": "/static/js/2.901c1cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c070c65f8c4092539f8",
+    "url": "/static/js/main.f5e3ab15.chunk.js"
+  },
+  {
+    "revision": "05b790e1b8b7107b52ed",
+    "url": "/static/js/runtime-main.1b9b8ecc.js"
+  }
+]);
